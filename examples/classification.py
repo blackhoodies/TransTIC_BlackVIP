f69a7cfbf40ae3bfefecd9a7df173bc9ea332b24
HongSheng416 <hongsheng.cs10g@nctu.edu.tw>
2024-06-01 21:35:03 +0800

    Add SPSA

--- examples/classification.py
+++ examples/classification.py
@@ -51,95 +51,8 @@ from torchvision.models import resnet50
 
 from compressai.datasets import ImageFolder
 from compressai.zoo import image_models
-
+from losses import *
 import yaml
-
-class RateDistortionLoss(nn.Module):
-    """Custom rate distortion loss with a Lagrangian parameter."""
-
-    def __init__(self, lmbda=1e-2):
-        super().__init__()
-        self.mse = nn.MSELoss()
-        self.lmbda = lmbda
-    
-    def psnr(self, output, target):
-        mse = torch.mean((output - target) ** 2)
-        if(mse == 0):
-            return 100
-        max_pixel = 1.
-        psnr = 10 * torch.log10(max_pixel / mse)
-        return torch.mean(psnr)
-
-    def forward(self, output, target):
-        N, _, H, W = target.size()
-        out = {}
-        num_pixels = N * H * W
-
-        out["bpp_loss"] = sum(
-            (torch.log(likelihoods).sum() / (-math.log(2) * num_pixels))
-            for likelihoods in output["likelihoods"].values()
-        )
-        out["mse_loss"] = self.mse(output["x_hat"], target)
-        out["rdloss"] = self.lmbda * 255**2 * out["mse_loss"] + out["bpp_loss"]
-        
-        out["psnr"] = self.psnr(torch.clamp(output["x_hat"],0,1), target)
-        return out
-
-
-class FeatureHook():
-    def __init__(self, module):
-        module.register_forward_hook(self.attach)
-    
-    def attach(self, model, input, output):
-        self.feature = output
-
-class Clsloss(nn.Module):
-    def __init__(self, device, perceptual_loss=False) -> None:
-        super().__init__()
-        self.ce = nn.CrossEntropyLoss()
-        self.classifier = resnet50(True)
-        self.classifier.requires_grad_(False)
-        self.hooks = [FeatureHook(i) for i in [
-            self.classifier.layer1,
-            self.classifier.layer2,
-            self.classifier.layer3,
-            self.classifier.layer4,
-        ]]
-        self.classifier = self.classifier.to(device)
-        for k, p in self.classifier.named_parameters():
-            p.requires_grad = False
-        self.classifier.eval()
-        self.perceptual_loss = perceptual_loss
-        self.transform = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-
-    def accuracy(output, target, topk=(1,)):
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
-
-    def forward(self, output, d, y_true):
-        x_hat = torch.clamp(output["x_hat"],0,1)
-        pred = self.classifier(self.transform(x_hat))
-        loss = self.ce(pred, y_true)
-        accu = sum(torch.argmax(pred,-1)==y_true)/pred.shape[0]
-        if self.perceptual_loss:
-            pred_feat = [i.feature.clone() for i in self.hooks]
-            _ = self.classifier(self.transform(d))
-            ori_feat = [i.feature.clone() for i in self.hooks]
-            perc_loss = torch.stack([nn.functional.mse_loss(p,o, reduction='none').mean((1,2,3)) for p,o in zip(pred_feat, ori_feat)])
-            perc_loss = perc_loss.mean()
-            return loss, accu, perc_loss
-
-        return loss, accu, None
 
 class AverageMeter:
     """Compute running average."""
@@ -210,27 +123,45 @@ def configure_optimizers(net, args):
 
 
 def train_one_epoch(
-    model, criterion_rd, criterion_cls, train_dataloader, optimizer, lmbda
+    model, approx_loss, train_dataloader, optimizer, lmbda, step
 ):
+    b1 = 0.9
+    a = 0.01
+    c = 0.01
+    gamma = 0.1
+    alpha = 0.4
+    o = 1.0
     model.train()
     device = next(model.parameters()).device
     tqdm_emu = tqdm.tqdm(enumerate(train_dataloader), total=len(train_dataloader), leave=False)
     for i, (d,l) in tqdm_emu:
         d = d.to(device)
         l = l.to(device)
+        ak = a/((step + o)**alpha)
+        ck = c/(step**gamma)
 
         optimizer.zero_grad()
 
         out_net = model(d)
+        w = torch.nn.utils.parameters_to_vector(model.g_a0_prompt.parameters())
 
-        out_criterion = criterion_rd(out_net, d)
-        loss, accu, perc_loss = criterion_cls(out_net, d, l)
-        total_loss = 1000*lmbda*perc_loss + out_criterion['bpp_loss']
-        total_loss.backward()
-        optimizer.step()
+        ghat, total_loss, accu, out_criterion, perc_loss, loss, model = approx_loss.spsa_grad_estimate_bi(w, model, d, l, lmbda, ck)
+        if step > 1:  
+            m1 = b1*m1 + ghat
+        else:              
+            m1 = ghat
+        accum_ghat = ghat + b1*m1
+         
+
+        #* param update
+        w_new = w - ak * accum_ghat
+        torch.nn.utils.vector_to_parameters(w_new, model.g_a0_prompt.parameters())
+        # total_loss.backward()
+        # optimizer.step()
 
         update_txt=f'[{i*len(d)}/{len(train_dataloader.dataset)}] | Loss: {total_loss.item():.3f} | MSE loss: {out_criterion["mse_loss"].item():.5f} | Bpp loss: {out_criterion["bpp_loss"].item():.4f}'
         tqdm_emu.set_postfix_str(update_txt, refresh=True)
+        step += 1
 
 def test_epoch(epoch, test_dataloader, model, criterion_rd, criterion_cls, lmbda, stage='test'):
     model.eval()
@@ -336,10 +267,11 @@ def main(argv):
 
     val_dataloader = DataLoader(val_dataset,batch_size=args.test_batch_size,num_workers=args.num_workers,shuffle=False,pin_memory=(device == "cuda"),)
     test_dataloader = DataLoader(test_dataset,batch_size=args.test_batch_size,num_workers=args.num_workers,shuffle=False,pin_memory=(device == "cuda"),)
-
+  
     net = image_models[args.model](quality=int(args.quality_level), prompt_config=args)
     net = net.to(device)
-
+    for param in net.parameters():
+        param.requires_grad = False
     if args.TRANSFER_TYPE == "prompt":
         for k, p in net.named_parameters():
             if "prompt" not in k:
@@ -349,6 +281,7 @@ def main(argv):
     lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30,60], gamma=0.5)
     rdcriterion = RateDistortionLoss(lmbda=args.lmbda)
     clscriterion = Clsloss(device, True)
+    approx_loss = Loss(model=net, device=device, lmbda=args.lmbda, perceptual_loss=True)
 
     last_epoch = 0
     if args.checkpoint: 
@@ -370,12 +303,12 @@ def main(argv):
     if args.TEST:
         best_loss = float("inf")
         tqrange = tqdm.trange(last_epoch, args.epochs)
-        loss = test_epoch(-1, test_dataloader, net, rdcriterion,clscriterion, args.VPT_lmbda,'test')
+        loss = test_epoch(-1, test_dataloader, net, rdcriterion, clscriterion,  args.VPT_lmbda,'test')
         return
 
     best_loss = float("inf")
     tqrange = tqdm.trange(last_epoch, args.epochs)
-    loss = test_epoch(-1, val_dataloader, net, rdcriterion,clscriterion, args.VPT_lmbda,'val')
+    # loss = test_epoch(-1, val_dataloader, net, rd_criterion, criterion_cls, args.VPT_lmbda,'val')
     for epoch in tqrange:
         train_dataloader = DataLoader(
         small_train_datasets[epoch%32],
@@ -384,15 +317,16 @@ def main(argv):
         shuffle=True,
         pin_memory=(device == "cuda"),
         )
+        step = args.batch_size*epoch + 1
         train_one_epoch(
             net,
-            rdcriterion,
-            clscriterion,
+            approx_loss,
             train_dataloader,
             optimizer,
-            args.VPT_lmbda
+            args.VPT_lmbda, 
+            step
         )
-        loss = test_epoch(epoch, val_dataloader, net, rdcriterion,clscriterion, args.VPT_lmbda,'val')
+        loss = test_epoch(epoch, val_dataloader, net, rdcriterion, clscriterion, args.VPT_lmbda,'val')
         lr_scheduler.step()
 
         is_best = loss < best_loss
@@ -417,4 +351,8 @@ def main(argv):
 
 
 if __name__ == "__main__":
+    # import compressai
+    # print(sys.path)
+    # print(os.path.abspath(compressai.__file__))
     main(sys.argv[1:])
+0
---
+++ examples/losses.py
@@ -0,0 +1,177 @@
+import torch
+import torch.nn as nn
+import math
+from torchvision.models import resnet50
+from torchvision import transforms
+
+
+class RateDistortionLoss(nn.Module):
+    """Custom rate distortion loss with a Lagrangian parameter."""
+
+    def __init__(self, lmbda=1e-2):
+        super().__init__()
+        self.mse = nn.MSELoss()
+        self.lmbda = lmbda
+    
+    def psnr(self, output, target):
+        mse = torch.mean((output - target) ** 2)
+        if(mse == 0):
+            return 100
+        max_pixel = 1.
+        psnr = 10 * torch.log10(max_pixel / mse)
+        return torch.mean(psnr)
+
+    def forward(self, output, target):
+        N, _, H, W = target.size()
+        out = {}
+        num_pixels = N * H * W
+
+        out["bpp_loss"] = sum(
+            (torch.log(likelihoods).sum() / (-math.log(2) * num_pixels))
+            for likelihoods in output["likelihoods"].values()
+        )
+        out["mse_loss"] = self.mse(output["x_hat"], target)
+        out["rdloss"] = self.lmbda * 255**2 * out["mse_loss"] + out["bpp_loss"]
+        
+        out["psnr"] = self.psnr(torch.clamp(output["x_hat"],0,1), target)
+        return out
+
+
+class FeatureHook():
+    def __init__(self, module):
+        module.register_forward_hook(self.attach)
+    
+    def attach(self, model, input, output):
+        self.feature = output
+
+class Clsloss(nn.Module):
+    def __init__(self, device, perceptual_loss=False) -> None:
+        super().__init__()
+        self.ce = nn.CrossEntropyLoss()
+        self.classifier = resnet50(True)
+        self.classifier.requires_grad_(False)
+        self.hooks = [FeatureHook(i) for i in [
+            self.classifier.layer1,
+            self.classifier.layer2,
+            self.classifier.layer3,
+            self.classifier.layer4,
+        ]]
+        self.classifier = self.classifier.to(device)
+        for k, p in self.classifier.named_parameters():
+            p.requires_grad = False
+        self.classifier.eval()
+        self.perceptual_loss = perceptual_loss
+        self.transform = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+    def accuracy(output, target, topk=(1,)):
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+    def forward(self, output, d, y_true):
+        x_hat = torch.clamp(output["x_hat"],0,1)
+        pred = self.classifier(self.transform(x_hat))
+        loss = self.ce(pred, y_true)
+        accu = sum(torch.argmax(pred,-1)==y_true)/pred.shape[0]
+        if self.perceptual_loss:
+            pred_feat = [i.feature.clone() for i in self.hooks]
+            _ = self.classifier(self.transform(d))
+            ori_feat = [i.feature.clone() for i in self.hooks]
+            perc_loss = torch.stack([nn.functional.mse_loss(p,o, reduction='none').mean((1,2,3)) for p,o in zip(pred_feat, ori_feat)])
+            perc_loss = perc_loss.mean()
+            return loss, accu, perc_loss
+
+        return loss, accu, None
+
+class Loss():
+    def __init__(self, model, device,  perceptual_loss=False, lmbda=1e-2):
+        super().__init__()
+        self.model = model
+        self.rd_loss = RateDistortionLoss(lmbda=lmbda)
+        self.cls_loss = Clsloss(device=device, perceptual_loss=perceptual_loss)
+    
+    def loss_fn(self, output, data, label, lmbda):
+        out_criterion = self.rd_loss(output, data)
+        loss, accu, perc_loss = self.cls_loss(output, data, label)
+        total_loss = 1000*lmbda*perc_loss + out_criterion['bpp_loss']
+        return total_loss, out_criterion, loss, accu, perc_loss
+        
+    def spsa_grad_estimate_bi(self, w, model, data, label, lmbda, ck):
+        #* repeat k times and average them for stabilizing
+        ghats = []
+        N_params = len(torch.nn.utils.parameters_to_vector(model.g_a0_prompt.parameters()))
+        sp_avg = 5
+        for spk in range(sp_avg):
+            #! Bernoulli {-1, 1}
+            # perturb = torch.bernoulli(torch.empty(self.N_params).uniform_(0,1)).cuda()
+            # perturb[perturb < 1] = -1
+            #! Segmented Uniform [-1, 0.5] U [0.5, 1]
+            p_side = (torch.rand(N_params).reshape(-1,1) + 1)/2
+            samples = torch.cat([p_side,-p_side], dim=1)
+            perturb = torch.gather(samples, 1, torch.bernoulli(torch.ones_like(p_side)/2).type(torch.int64)).reshape(-1).cuda()
+            del samples; del p_side
+
+            #* two-side Approximated Numerical Gradient
+            w_r = w + ck*perturb
+            w_l = w - ck*perturb
+            torch.nn.utils.vector_to_parameters(w_r, self.model.g_a0_prompt.parameters())
+            output1 = model(data)
+            torch.nn.utils.vector_to_parameters(w_l, self.model.g_a0_prompt.parameters())
+            output2 = model(data)
+            # torch.nn.utils.vector_to_parameters(w_r, self.model.coordinator.dec.parameters())
+            # torch.nn.utils.vector_to_parameters(w_l, self.model.coordinator.dec.parameters())
+            
+            total_loss1, out_criterion1, loss1, accu1, perc_loss1 = self.loss_fn(output1, data, label, lmbda)
+            total_loss2, out_criterion2, loss2, accu2, perc_loss2 = self.loss_fn(output2, data, label, lmbda)
+
+            #* parameter update via estimated gradient
+            ghat = (total_loss1 - total_loss2)/((2*ck)*perturb)
+            ghats.append(ghat.reshape(1, -1))
+        if sp_avg == 1: pass
+        else: ghat = torch.cat(ghats, dim=0).mean(dim=0) 
+        total_loss = ((total_loss1 + total_loss2)/2)
+        acc = (0.5*(accu1 + accu2)).item()
+
+        return ghat, total_loss, acc, out_criterion1, perc_loss1, loss1, model
+    
+
+    
+def compute_accuracy(output, target, topk=(1, )):
+    """Computes the accuracy over the k top predictions for
+    the specified values of k.
+
+    Args:
+        output (torch.Tensor): prediction matrix with shape (batch_size, num_classes).
+        target (torch.LongTensor): ground truth labels with shape (batch_size).
+        topk (tuple, optional): accuracy at top-k will be computed. For example,
+            topk=(1, 5) means accuracy at top-1 and top-5 will be computed.
+
+    Returns:
+        list: accuracy at top-k.
+    """
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    if isinstance(output, (tuple, list)):
+        output = output[0]
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        acc = correct_k.mul_(100.0 / batch_size)
+        res.append(acc)
+
+    return res
